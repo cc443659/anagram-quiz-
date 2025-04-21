4c771e2128cef84f64947122a696dc4e3d7c8100
cc443659 <cc443659@truro-penwith.ac.uk>
2025-04-21 16:59:22 +0100

    upload files via git

--- script.js
+++ script.js
@@ -1,68 +1,89 @@
-        let questions = ["Voices Rant On", "Here come dots", "Swear oft", "Im a dot in place", "Dog inc"];
-        let answers = ["Conversation", "Morse code", "Software", "A decimal point", "Coding"];
+let questions = [
+    "Voices rant on",
+    "Here come dots",
+    "Swear oft",
+    "Im a dot in place",
+    "Dog inc"
+];
+let answers = [
+    "Conversation",
+    "Morse code",
+    "Software",
+    "A decimal point",
+    "Coding"
+];
 
-        let count = 0;
+let i = 1; // index, or count
+let userAnswers = [];
 
-        function quizStart() {
-            document.getElementById("leadP").innerHTML = 'Solve the following anagrams.';
-            document.getElementById("extra").innerHTML = 'Use the <span style="font-weight: 700;">Enter</span> key to move on to the next question.';
-        }
+function quizStart() {
+    document.getElementById("startButton").innerHTML = null;
 
-        function getQuestion() {
-            let answerInputElement_old = document.getElementById("answer");
-            if (answerInputElement_old) {
-                answerInputElement_old.removeEventListener("keydown", handleEnterKey);
-            }
-            
-            document.getElementById("questions").innerHTML += questions[count];
-            document.getElementById("questions").innerHTML += ' <input autocomplete="off" id="answer"/> <br />';
-            
-            let answerInputElement = document.getElementById("answer");
-            answerInputElement.addEventListener("keydown", handleEnterKey);
-        }
-        
-        function handleEnterKey(event) {
-        if (event.key === "Enter") {
-            if (count < 4) {            
-                let inputValue = document.getElementById("answer").value;
-                console.log(inputValue[count]);
-                count += 1;
-                
-                document.getElementById("questions").innerHTML = "";
-                
-                getQuestion();
-            } else {
-                document.getElementById("questions").innerHTML = "Quiz complete.";
-                setTimeout(() => console.log("Quiz complete."), 1800);
+    document.getElementById("leadP").innerHTML = 'Solve the following anagrams.';
+    document.getElementById("extra").innerHTML = 'Use the <span style="font-weight: 700;">Enter</span> key to move on to the next question.';
 
-                document.getElementById("leadP").innerHTML = null;
-                document.getElementById("extra").innerHTML = null;
-                
-                calcResultsAnim();
-            }
-        }
+    getQuestion();
+}
 
-    }
-
-    function calcResultsAnim() {
-        setTimeout(() => document.getElementById("questions").innerHTML = "Calculating results, please wait...", 2000);
-        setTimeout(() => document.getElementById("questions").innerHTML = "Calculating results, please wait.", 2500);
-        setTimeout(() => document.getElementById("questions").innerHTML = "Calculating results, please wait..", 3000);
-        setTimeout(() => document.getElementById("questions").innerHTML = "Calculating results, please wait...", 3500);
-        setTimeout(() => document.getElementById("questions").innerHTML = "", 4000);
-        setTimeout(() => displayResults(), 5500);
-    }
-
-    function buttonHide() {
-        document.getElementById("startButton").innerHTML = null;
-    }
-
-    let box = document.getElementById("results");
+function getQuestion() {
+    document.getElementById("questions").innerHTML += questions[i - 1];
+    document.getElementById("questions").innerHTML += ' <input autocomplete="off" id="answer"/> <br />';
     
-    function displayResults() {
-            box.style.display = "inline-block";
-            box.style.animation = "movein 0.5s linear 1";
-            
-            let userAnswer1 = inputValue[1];
-            document.getElementById("userAnswer1").innerHTML = userAnswer1.value;
+    let answerInputElement = document.getElementById("answer");
+    answerInputElement.addEventListener("keydown", handleEnterKey);
+}
+
+function handleEnterKey(event) {
+    if (event.key === "Enter") {
+        let inputValue = document.getElementById("answer").value;
+
+        console.log(`${inputValue} : ${i}`) // debugging purposes
+
+        userAnswers.push(inputValue)
+        if (i < questions.length) {
+            // determines whether quiz is over or not
+            document.getElementById("questions").innerHTML = "";
+            i += 1;
+
+            getQuestion();
+        } else {
+            // if quiz is now finished...
+            document.getElementById("questions").innerHTML = "Quiz complete.";
+
+            document.getElementById("leadP").innerHTML = null;
+            document.getElementById("extra").innerHTML = null;
+
+            calcResultsAnim();
+        }
     }
+}
+
+function calcResultsAnim() {
+    for (let i = 1; i < 4; i++) {
+        setTimeout(() => document.getElementById("questions").innerHTML = `Calculating results, please wait${".".repeat(i)}`, (2000 + (i * 500)));
+    }
+    setTimeout(() => document.getElementById("questions").innerHTML = " ", 4000);
+    setTimeout(() => displayResults(), 5500);
+    // uses equivalent of the logic in time.sleep() within Python's time module
+    //
+    // not ideal for bigger animations, in which case CSS should be used whenever possible.
+}
+
+
+function displayResults() {
+    // show which answers the user got [in]correct
+    let box = document.getElementById("results"); // reduces code redundancy below
+    box.style.display = "inline-block";
+    box.style.animation = "movein 0.5s linear 1";
+
+    for (let i = 0; i < 5; i++) {
+        let userAnswerP = document.getElementById(`userAnswer${i + 1}`);
+        userAnswerP.innerHTML = userAnswers[i];
+        if (userAnswers[i] == answers[i]) {
+            userAnswerP.innerHTML += "<br /><span style='color: green; font-weight: 700;'>Correct</span>";
+        } else {
+            userAnswerP.innerHTML += "<br /><span style='color: red; font-weight: 700;'>Incorrect</span>";
+            userAnswerP.innerHTML += `<br />Correct answer: ${answers[i]}`;
+        }
+    }
+}
